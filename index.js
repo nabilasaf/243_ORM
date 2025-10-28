ab98d77a5b153a764675d923d2bf96e9b4d05f40
nabilasaf <nabilasafitri861@gmail.com>
2025-10-28 09:58:41 +0700

    menambahkan code di index js

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const db = require('./models');
+const PORT = 3000;
+app.use(express.json());
+app .use(express.urlencoded({ extended: true }));
+app.listen(PORT, () => {
+    console.log(`Server started on port 3000`);
+});
+db.sequelize.sync()
+.then((result)=> {
+    app.listen(3000, ()=> {
+        console.log('Server started');
+    })
+})
+.catch((err) => {
+    console.log(err);
+});
