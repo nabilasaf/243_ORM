3b51692b61b919835e46d5a4e45b4b938954403c
nabilasaf <nabilasafitri861@gmail.com>
2025-10-28 09:50:17 +0700

    mengkoneksikan ke database dan menambahkan isi di komik js

---
+++ models/komik.js
@@ -0,0 +1,13 @@
+module.exports = (sequelize, DataTypes) => {
+    const Komik = sequelize.define("Komik", {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+        },
+    });
+    return Komik;
+};
